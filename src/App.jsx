d54e715d5a381c2582ad1e9f6a9bac2d3727527c
emaduddin678 <emaduddin678@gmail.com>
2024-02-13 02:49:39 +0600

    first page done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Page3 from "./pages/page-3/Page3";
+
+function App() {
+  return (
+    <>
+      <Page3 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/page-3/Page3.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import bannertia from "../../assets/bannerwithitial.png";
+import logo from "../../assets/Break-The-Box-Content.png";
+import { TypeAnimation } from "react-type-animation";
+
+const Page3 = () => {
+  return (
+    <>
+      <section className="relative bg-[#F4ECE1] bg-[url('/src/assets/zodiac-01-01.png')] bg-cover bg-center bg-no-repeat bgbg bg-fixed">
+        <div className="mx-auto max-w-screen-xl px-4  flex h-screen items-center">
+          <div className="mx-auto max-w-7xl text-center ">
+            <div className="mx-auto xl:h-[430px] lg:h-[350px] md:h-[250px] h-[130px] overflow-hidden">
+              <img src={bannertia} className="mx-auto " alt="brandImage" />
+            </div>
+
+            <p className="mt-4 w-[100%] textgradient text-xl md:text-3xl lg:text-4xl emad bg-gradient-to-t from-[#eb475c] to-[#fba209] bg-clip-text font-extrabold text-transparent ">
+              <TypeAnimation
+                style={{
+                  whiteSpace: "pre-line",
+                  height: "100px",
+                  display: "block",
+                }}
+                className="lg:h-[100px] lg:text-4xl "
+                sequence={[
+                  `“Your horoscope advises you to\n wear pants today. Trust us,it's for the best”`,
+                  10,
+                  "",
+                ]}
+                omitDeletionAnimation={false}
+              />
+            </p>
+          </div>
+        </div>
+        {/* “Your horoscope advises you to <br /> wear pants today. Trust us,
+              it's for the best” */}
+        <img
+          src={logo}
+          className=" lg:w-24 md:w-20 w-12 absolute bottom-8 right-8"
+          alt="logo"
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page3;
