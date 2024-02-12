aaa76cf3edd3ba4db0611e4697c4126aaf5eca25
emaduddin678 <emaduddin678@gmail.com>
2024-02-13 03:28:44 +0600

    animation dione

--- src/pages/page-3/Page3.jsx
+++ src/pages/page-3/Page3.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import bannertia from "../../assets/bannerwithitial.png";
-import logo from "../../assets/Break-The-Box-Content.png";
 import { TypeAnimation } from "react-type-animation";
+import bannertia from "../../assets/bannerwithitial.png";
+import name from "../../assets/Fortune Teller-01.png";
+import tia from "../../assets/tia_00182.png";
+import logo from "../../assets/Break-The-Box-Content.png";
 
 const Page3 = () => {
   return (
@@ -9,11 +11,22 @@ const Page3 = () => {
       <section className="relative bg-[#F4ECE1] bg-[url('/src/assets/zodiac-01-01.png')] bg-cover bg-center bg-no-repeat bgbg bg-fixed">
         <div className="mx-auto max-w-screen-xl px-4  flex h-screen items-center">
           <div className="mx-auto max-w-7xl text-center ">
-            <div className="mx-auto xl:h-[430px] lg:h-[350px] md:h-[250px] h-[130px] overflow-hidden">
-              <img src={bannertia} className="mx-auto " alt="brandImage" />
+            <div className="relative mx-auto xl:h-[430px] lg:h-[350px] md:h-[250px] h-[130px]  ">
+              {/* <div className="mx-auto xl:h-[430px] lg:h-[350px] md:h-[250px] h-[130px] overflow-hidden"> */}
+              <img
+                src={tia}
+                className="absolute sm:w-96 md:w-auto -top-48 md:-top-96 md:-left-60 -left-32  z-50"
+                alt=""
+              />
+              <img
+                src={name}
+                className="absolute  -top-14 md:-top-32  z-50"
+                alt=""
+              />
+              {/* <img src={bannertia} className="mx-auto " alt="brandImage" /> */}
             </div>
 
-            <p className="mt-4 w-[100%] textgradient text-xl md:text-3xl lg:text-4xl emad bg-gradient-to-t from-[#eb475c] to-[#fba209] bg-clip-text font-extrabold text-transparent ">
+            <p className=" w-[100%] textgradient text-xl md:text-3xl lg:text-4xl emad bg-gradient-to-t from-[#eb475c] to-[#fba209] bg-clip-text font-extrabold text-transparent ">
               <TypeAnimation
                 style={{
                   whiteSpace: "pre-line",
@@ -23,10 +36,8 @@ const Page3 = () => {
                 className="lg:h-[100px] lg:text-4xl "
                 sequence={[
                   `“Your horoscope advises you to\n wear pants today. Trust us,it's for the best”`,
-                  10,
-                  "",
+                  1000,
                 ]}
-                omitDeletionAnimation={false}
               />
             </p>
           </div>
